bb05b5fd42dde1aa4df1e22695a66a95dcd9e693
Chih-Hung Hsieh <chh@google.com>
2016-06-29 14:22:39 -0700

    Fix misc-macro-parentheses warnings in libbcc.
    Use NOLINT to suppress wrong clang-tidy warnings
    on type names used in a macro.
    Bug: 28705665
    Test: build with clang-tidy
    
    Change-Id: Id9197d40258838245931bc0a0a00e8287ec0a764

--- include/bcinfo/Wrap/support_macros.h
+++ include/bcinfo/Wrap/support_macros.h
@@ -22,14 +22,15 @@
 // Define macro, to use within a class declaration,  to disallow constructor
 // copy. Defines copy constructor declaration under the assumption that it
 // is never defined.
+// NOLINT: Do not add parentheses around 'class_name'.
 #define DISALLOW_CLASS_COPY(class_name) \
-  class_name(class_name& arg)  // Do not implement
+  class_name(class_name& arg)  // NOLINT, Do not implement
 
 // Define macro, to use within a class declaration,  to disallow assignment.
 // Defines assignment operation declaration under the assumption that it
 // is never defined.
 #define DISALLOW_CLASS_ASSIGN(class_name) \
-  void operator=(class_name& arg)  // Do not implement
+  void operator=(class_name& arg)  // NOLINT, Do not implement
 
 // Define macro to add copy and assignment declarations to a class file,
 // for which no bodies will be defined, effectively disallowing these from
